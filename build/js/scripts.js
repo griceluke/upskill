48ecf268444198ba47a5dfc98e701238ab88f6a9
Luke Grice <griceluke@outlook.com>
2020-12-07 21:29:17 +0000

    Gulp css set up - Reset build directory folder - Autoprefix - Minify - Rename to .min.css - Remove unused css

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,8 +1,12 @@
-
 const giveAlert = (string) => {
   console.log(string);
   alert(`Alert given ${string}`);
 }
+
+const changeBackgroundColor = () => {
+  document.getElementById('body').classList.add("red");
+}
+
 document.addEventListener("DOMContentLoaded", function() {
   
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,61 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
+// Import the dependencies needed
+const gulp = require('gulp'),
+      autoprefixer = require('gulp-autoprefixer'),
+      del = require('del'),
+      // sass = require('gulp-sass'),
+      cleanCSS = require('gulp-clean-css'),
+      rename = require('gulp-rename'),
+      purgecss = require('gulp-purgecss');
+      // browserSync = require('browser-sync').create();
+
+// Set where to find the files/folders and what to rename them
+const folders = {
+  build: {
+    main: 'build',
+    css: 'css',
+    js: 'js',
+  },
+  prod: {
+    main: 'prod',
+    css: 'styles',
+    js: 'scripts',
+  },
 }
 
-exports.default = defaultTask
+// Clears the production folders and files before re-building
+const clear = () => {
+  return del(['prod/**']);
+};
+
+// Runs the processes on styling
+const css = (cb) => {
+  gulp.src(`./${folders.build.main}/${folders.build.css}/styles.css`)
+    // 1st - Prefix css for browser compatibility
+    .pipe(autoprefixer())
+    // 2nd - Minify css to a single line and imported files code into file itself
+    .pipe(cleanCSS({debug: true, compatibility: 'ie8'}))
+    // 3rd - Rename file to {filename}.min.css
+    .pipe(rename(function(path) {
+      path.extname = ".min.css";
+    }))
+    // 4th - Delete unused css styles - WARNING a pain for dynamic class names. 
+    .pipe(purgecss({content: [`./${folders.build.main}/**/*.html`]}))
+    // - Push new code into distributable folder
+    .pipe(gulp.dest(`./${folders.prod.main}/${folders.prod.css}/`))      
+    // .pipe(browserSync.stream());
+  cb();
+};
+
+// Runs the processes on scripts
+const js = (cb) => {
+ 
+  cb();
+};
+
+// In series (one after the other)
+// Clears first
+// Runs css and js builds in parallel as they shouldnt need to rely on one-another
+exports.default = gulp.series(
+  clear, 
+  gulp.parallel(css, js)
+);
